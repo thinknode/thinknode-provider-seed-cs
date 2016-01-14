fafbb84120bec30db5b595245f15de19b6e1e14f
kburnett <kburnett@thinknode.com>
2016-01-14 16:40:53 -0500

    working on app seed

--- App/App.cs
+++ App/App.cs
@@ -1,9 +1,96 @@
 ﻿using System;
+using MsgPack;
 using MsgPack.Serialization;
 using Thinknode;
 
 namespace App
 {
+    /// <summary>
+    /// Built-in datetime serializers.
+    /// </summary>
+    public class DateTimeSerializer : MessagePackSerializer<DateTime>
+    {
+        public DateTimeSerializer(SerializationContext ownerContext) : base(ownerContext) {}
+
+        /// <summary>
+        /// Defines the serialization for the Datetime.
+        /// </summary>
+        /// <param name="packer">Packer.</param>
+        /// <param name="objectTree">Object tree.</param>
+        protected override void PackToCore(Packer packer, DateTime objectTree)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            double ticks = (TimeZoneInfo.ConvertTimeToUtc(objectTree) - epoch).TotalMilliseconds;
+            byte[] bytes;
+            try
+            {
+                sbyte val = Convert.ToSByte(ticks);
+                bytes = BitConverter.GetBytes(val);
+            }
+            catch (OverflowException)
+            {
+                try
+                {
+                    short val = Convert.ToInt16(ticks);
+                    bytes = BitConverter.GetBytes(val);
+                }
+                catch (OverflowException)
+                {
+                    try
+                    {
+                        int val = Convert.ToInt32(ticks);
+                        bytes = BitConverter.GetBytes(val);
+                    }
+                    catch (OverflowException)
+                    {
+                        long val = Convert.ToInt64(ticks);
+                        bytes = BitConverter.GetBytes(val);
+                    }
+                }
+            }
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            packer.PackExtendedTypeValue(1, bytes);
+        }
+
+        /// <summary>
+        /// Defines the deserialization for the Datetime.
+        /// </summary>
+        /// <returns>The from core.</returns>
+        /// <param name="unpacker">Unpacker.</param>
+        protected override DateTime UnpackFromCore(Unpacker unpacker)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            MessagePackObject obj = unpacker.LastReadData;
+            MessagePackExtendedTypeObject ext = obj.AsMessagePackExtendedTypeObject();
+            byte[] bytes = ext.GetBody();
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            double val;
+            if (bytes.Length == 1)
+            {
+                val = Convert.ToDouble(bytes[0]);
+            }
+            else if (bytes.Length == 2)
+            {
+                val = Convert.ToDouble(BitConverter.ToInt16(bytes, 0));
+            }
+            else if (bytes.Length == 4)
+            {
+                val = Convert.ToDouble(BitConverter.ToInt32(bytes, 0));
+            }
+            else
+            {
+                val = Convert.ToDouble(BitConverter.ToInt64(bytes, 0));
+            }
+            return epoch.AddMilliseconds(val);
+        }
+    }
+
     class App : Provider
     {
         // ***
@@ -47,6 +134,16 @@ namespace App
             return arr.Length;
         }
 
+        /// <summary>
+        /// Gets the year component of the given datetime value.
+        /// </summary>
+        /// <returns>The year.</returns>
+        /// <param name="val">The datetime value.</param>
+        public static uint GetYear(DateTime val)
+        {
+            return Convert.ToUInt32(val.Year);
+        }
+
         // ***
         // REPLACE THE METHODS WITHIN THESE MARKERS WITH THE METHODS DEFINED IN YOUR MANIFEST.
         // ***
@@ -61,8 +158,8 @@ namespace App
             // Create App instance.
             App app = new App();
 
-            // Add Serializers (DELETE OR MODIFY THIS LINE)
-            app.context.Serializers.RegisterOverride(new PersonSerializer(app.context));
+            // Add Serializers (Add your custom serializers below this line)
+            app.context.Serializers.RegisterOverride(new DateTimeSerializer(app.context));
 
             // Start app.
             app.Start();
--- App/MyClass.cs
+++ App/MyClass.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using MsgPack;
-using MsgPack.Serialization;
 
 namespace App
 {
@@ -8,6 +6,10 @@ namespace App
     {
         public String name;
         public int age;
+
+        public Person()
+        {
+        }
 
         public Person(String n, int a)
         {
@@ -18,33 +20,6 @@ namespace App
         public override string ToString()
         {
             return string.Format("name: {0}; age: {1}", name, age);
-        }
-    }
-
-    public class PersonSerializer : MessagePackSerializer<Person>
-    {
-        public PersonSerializer(SerializationContext ownerContext) : base(ownerContext) {}
-
-        protected override void PackToCore(Packer packer, Person objectTree)
-        {
-            packer.PackMapHeader(2);
-            packer.Pack("name");
-            packer.Pack(objectTree.name);
-            packer.Pack("age");
-            packer.Pack(objectTree.age);
-        }
-
-        protected override Person UnpackFromCore(Unpacker unpacker)
-        {
-            MessagePackObject obj = unpacker.UnpackSubtreeData();
-            MessagePackObjectDictionary dict = obj.AsDictionary();
-            MessagePackObject objName = new MessagePackObject();
-            MessagePackObject objAge = new MessagePackObject();
-            dict.TryGetValue("name", out objName);
-            dict.TryGetValue("age", out objAge);
-            String name = objName.AsStringUtf8();
-            int age = objAge.AsInt32();
-            return new Person(name, age);
         }
     }
 }
